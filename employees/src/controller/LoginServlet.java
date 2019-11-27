25ffd943e9ea4907a472e173e86ac73b7668a660
GD3 <GD3@DESKTOP-H9MC3L3>
2019-11-27 17:05:36 +0900

    test용도로 empNo에 1입력할 경우 로그인이 되도록 설정

--- employees/src/controller/LoginServlet.java
+++ employees/src/controller/LoginServlet.java
@@ -39,9 +39,16 @@ public class LoginServlet extends HttpServlet {
 			HttpSession session = request.getSession();
 			//입력한 값이 데이터베이스상에 있는지 여부를 확인 없으면 0이,존재하면 1이 리턴된다
 				if(employeesDao.login(employees)==0) {
+					if(empNo==1) {//test용 설정 emp값만 1이면 로그인 가능하다.
+						//리턴값이 0이 아닐경우 session에 직원번호 값을 저장한다.
+						sessionEmpNo=employeesDao.login(employees);
+						session.setAttribute("sessionEmpNo", sessionEmpNo);
+						request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
+						return ;
+					}
 					System.out.println("LOGIN 실패 다시입력");
 					response.sendRedirect(request.getContextPath()+"/login");
-				}else {
+				}else{
 					//리턴값이 0이 아닐경우 session에 직원번호 값을 저장한다.
 					sessionEmpNo=employeesDao.login(employees);
 					session.setAttribute("sessionEmpNo", sessionEmpNo);
